b8e6e0e22938efd85b24fad354b0affbc47c1d99
neosy <y.savonin@dteam.site>
2025-11-09 03:04:11 +0300

    Refactor yt-dlp download options: prefer MP4 + M4A, fallback to best available feat(download): add support for M4A audio and original video/audio formats

--- internal/domain/download/options.go
+++ internal/domain/download/options.go
@@ -7,6 +7,12 @@ type DownloadOptions struct {
 	// Type of content to download (video, audio, or both)
 	FormatType dtypes.FormatType
 
+	// Video format (orig, mp4)
+	VideoFormat *dtypes.VideoFormat
+
+	// Audio format (orig, mp3)
+	AudioFormat *dtypes.AudioFormat
+
 	// Custom output directory
 	DownloadsDir *string
 
