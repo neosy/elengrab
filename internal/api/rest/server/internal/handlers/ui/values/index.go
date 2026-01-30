da8c3f1016de467bb38d9a3c2dde439f13e3d516
Neosy <neosy.dev@gmail.com>
2026-01-30 20:32:13 +0300

    Chore: Update CSS cache version

--- internal/api/rest/server/internal/handlers/ui/values/index.go
+++ internal/api/rest/server/internal/handlers/ui/values/index.go
@@ -1,11 +1,14 @@
 package uivalues
 
+import iconfig "github.com/neosy/elengrab/infrastructure/config"
+
 const (
 	pageTitle = "Elengrab — Fast YouTube Downloader"
 	header    = "Elengrab"
 )
 
 var IndexValues = map[string]any{
-	"Title":  pageTitle,
-	"Header": header,
+	"Title":      pageTitle,
+	"Header":     header,
+	"AppVersion": iconfig.AppVersion,
 }
