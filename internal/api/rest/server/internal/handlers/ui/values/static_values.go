66cbb7e4b10489516207910699a72afbe768c4e3
Neosy <neosy.dev@gmail.com>
2026-02-04 19:16:49 +0300

    Style: Change the title in the index. Closes #193

--- internal/api/rest/server/internal/handlers/ui/values/static_values.go
+++ internal/api/rest/server/internal/handlers/ui/values/static_values.go
@@ -3,16 +3,18 @@ package uivalues
 import iconfig "github.com/neosy/elengrab/infrastructure/config"
 
 const (
-	pageTitle            = "Elengrab — Fast YouTube Downloader"
+	pageTitle            = "Elengrab — Fast Media Downloader | YouTube, VK Video and more"
+	pageDescription      = "Download video and audio from YouTube, Facebook, Instagram, VK, TikTok, and more with Elengrab."
 	header               = "Elengrab"
 	inputGrabPlaceholder = "Enter video or audio URL"
 )
 
 var (
 	IndexValues = map[string]any{
-		"Title":      pageTitle,
-		"Header":     header,
-		"AppVersion": iconfig.AppVersion,
+		"Title":       pageTitle,
+		"Description": pageDescription,
+		"Header":      header,
+		"AppVersion":  iconfig.AppVersion,
 	}
 
 	FormGrabValues = map[string]any{
