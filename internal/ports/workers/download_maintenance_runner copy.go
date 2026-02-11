80f852f82a1768095ed6187939ff3fd5f532e8a9
Neosy <neosy.dev@gmail.com>
2026-02-11 14:06:21 +0300

    Feature: Change the history sorting date to the upload date. Closes #183

--- internal/ports/workers/download_maintenance_runner copy.go
+++ internal/ports/workers/download_maintenance_runner copy.go
@@ -2,6 +2,7 @@ package pworkers
 
 import "context"
 
+// DownloadMaintenanceRunner defines the interface for a download maintenance runner.
 type DownloadMaintenanceRunner interface {
 	UpdateHash(ctx context.Context) error
 	DeleteDuplicates(ctx context.Context) error
