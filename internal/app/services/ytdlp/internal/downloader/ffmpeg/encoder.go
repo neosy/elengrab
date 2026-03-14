e72f71c109664e7417d89875479f5dfb11b1bf7f
Neosy <neosy.dev@gmail.com>
2026-03-14 13:25:24 +0300

    Refactor: Simplify transcoding parameter selection

--- internal/app/services/ytdlp/internal/downloader/ffmpeg/encoder.go
+++ internal/app/services/ytdlp/internal/downloader/ffmpeg/encoder.go
@@ -6,10 +6,13 @@ const (
 	defaultVideoH264 = "libx264 -crf 22 -preset slow"
 	defaultVideoH265 = "libx265 -crf 22 -preset slow"
 	defaultVideoAV1  = "libaom-av1 -crf 0 -b:v 0"
+
+	defaultAudioAAC  = "aac -b:a 160k"
+	defaultAudioOPUS = "libopus" // -b:a 160k
 )
 
-func VideoEncoderArgs(videoCode dtypes.VideoCodec) string {
-	switch videoCode {
+func VideoEncoderArgs(videoCodec dtypes.VideoCodec) string {
+	switch videoCodec {
 	case dtypes.VideoCodecH264:
 		return defaultVideoH264
 	case dtypes.VideoCodecAV1:
@@ -18,5 +21,16 @@ func VideoEncoderArgs(videoCode dtypes.VideoCodec) string {
 		return defaultVideoH265
 	default:
 		return defaultVideoH264
+	}
+}
+
+func AudioEncoderArgs(audioCodec dtypes.AudioCodec) string {
+	switch audioCodec {
+	case dtypes.AudioCodecAAC:
+		return defaultAudioAAC
+	case dtypes.AudioCodecOPUS:
+		return defaultAudioOPUS
+	default:
+		return defaultAudioAAC
 	}
 }
--- internal/app/services/ytdlp/internal/downloader/helper/consts.go
+++ internal/app/services/ytdlp/internal/downloader/helper/consts.go
@@ -14,6 +14,7 @@ const (
 	audioQualityMP3Default         = "2"
 	audioQualityMP3BitrateDefault  = 192
 	audioQualityM4ADefault         = "2"
-	audioQualityOPUSDefault        = "160K"
+	audioQualityAACBitrateDefault  = "160k"
+	audioQualityOPUSDefault        = "160k"
 	audioQualityFLACBitrateDefault = 965
 )
