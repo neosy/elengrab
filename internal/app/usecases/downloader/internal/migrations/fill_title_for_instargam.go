9bbba4a73d04af8171f9b300d3a1a2cb7d8bee8c
Neosy <neosy.dev@gmail.com>
2026-05-12 18:51:54 +0300

    Build: Add 'fill title for instagram' migration

---
+++ internal/app/usecases/downloader/internal/migrations/fill_title_for_instargam.go
@@ -0,0 +1,64 @@
+package migrations
+
+import (
+	"context"
+	"fmt"
+
+	hostdetect "github.com/neosy/elengrab/internal/app/utils/host_detect"
+	ddownload "github.com/neosy/elengrab/internal/domain/download"
+)
+
+func (m *migrations) fillTitleForInstagram(ctx context.Context) (bool, error) {
+	medias, err := m.getAllDownloads(ctx, false,
+		func(download *ddownload.MediaDownload) bool {
+			if hostdetect.Instagram(download.MediaURL) {
+				return true
+			}
+			return false
+		},
+	)
+	if err != nil {
+		return false, err
+	}
+
+	if len(medias) == 0 {
+		return true, nil
+	}
+
+	m.logger.Debug("Found instagram medias", "count", len(medias))
+
+	var hasError bool
+
+	for i, media := range medias {
+		m.logger.Debug("Extract media info", "index", i+1, "total", len(medias))
+
+		info, err := m.services.downloader.GetInfo(ctx, media.MediaURL, false)
+		if err != nil {
+			continue
+		}
+
+		if info.Title == "" || media.MediaTitle == info.Title {
+			continue
+		}
+
+		err = m.usecases.download.Patch(
+			ctx, nil,
+			media.DownloadID,
+			func(download *ddownload.MediaDownload) {
+				download.MediaTitle = info.Title
+				if info.Description != "" && (download.MediaDescription == nil || *download.MediaDescription != info.Description) {
+					download.MediaDescription = &info.Description
+				}
+			},
+		)
+		if err != nil {
+			hasError = true
+		}
+	}
+
+	if hasError {
+		return false, fmt.Errorf("errors in the migration process")
+	}
+
+	return true, nil
+}
