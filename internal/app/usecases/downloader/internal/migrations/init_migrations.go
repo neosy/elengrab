e98225551788e9dca467a69e6bc4ea0fc7216658
Neosy <neosy.dev@gmail.com>
2026-05-12 18:51:54 +0300

    Build: Add 'fill title for instagram' migration

--- internal/app/usecases/downloader/internal/migrations/init_migrations.go
+++ internal/app/usecases/downloader/internal/migrations/init_migrations.go
@@ -4,4 +4,5 @@ func (m *migrations) initMigrations() {
 	m.migrationIDs.addMigration("01_move_downloads_to_storage", m.moveDownloadsToStorage)
 	m.migrationIDs.addMigration("02_fill_media_info", m.fillMediaInfo)
 	m.migrationIDs.addMigration("03_fill_thumbnails", m.fillThumbnails)
+	m.migrationIDs.addMigration("04_fill_title_for_instagram", m.fillTitleForInstagram)
 }
