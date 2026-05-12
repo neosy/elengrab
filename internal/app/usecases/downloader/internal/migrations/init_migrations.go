5ac751aeb3f006ed348f895b8edff14f7936c5fc
Neosy <neosy.dev@gmail.com>
2026-05-12 19:04:27 +0300

    Build: Add 'fill media description' migration

--- internal/app/usecases/downloader/internal/migrations/init_migrations.go
+++ internal/app/usecases/downloader/internal/migrations/init_migrations.go
@@ -5,4 +5,5 @@ func (m *migrations) initMigrations() {
 	m.migrationIDs.addMigration("02_fill_media_info", m.fillMediaInfo)
 	m.migrationIDs.addMigration("03_fill_thumbnails", m.fillThumbnails)
 	m.migrationIDs.addMigration("04_fill_title_for_instagram", m.fillTitleForInstagram)
+	m.migrationIDs.addMigration("05_fill_media_description", m.fillMediaDescription)
 }
