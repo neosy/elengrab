c7f15964d005aeac083c5bf35dab17da6dde11b6
Neosy <neosy.dev@gmail.com>
2026-05-21 10:10:08 +0300

    Refactor: Split required and deferred migrations execution flow

--- internal/app/usecases/downloader/internal/migrations/init_migrations.go
+++ internal/app/usecases/downloader/internal/migrations/init_migrations.go
@@ -1,9 +1,10 @@
 package migrations
 
 func (m *migrations) initMigrations() {
-	m.migrationIDs.addMigration("01_move_downloads_to_storage", m.moveDownloadsToStorage)
-	m.migrationIDs.addMigration("02_fill_media_info", m.fillMediaInfo)
-	m.migrationIDs.addMigration("03_fill_thumbnails", m.fillThumbnails)
-	m.migrationIDs.addMigration("04_fill_title_for_instagram", m.fillTitleForInstagram)
-	m.migrationIDs.addMigration("05_fill_media_description", m.fillMediaDescription)
+	m.requiredMigrationIDs.addMigration("01_move_downloads_to_storage", m.moveDownloadsToStorage)
+
+	m.deferredMigrationIDs.addMigration("02_fill_media_info", m.fillMediaInfo)
+	m.deferredMigrationIDs.addMigration("03_fill_thumbnails", m.fillThumbnails)
+	m.deferredMigrationIDs.addMigration("04_fill_title_for_instagram", m.fillTitleForInstagram)
+	m.deferredMigrationIDs.addMigration("05_fill_media_description", m.fillMediaDescription)
 }
---
+++ internal/app/usecases/downloader/internal/migrations/run_migrations.go
@@ -0,0 +1,60 @@
+package migrations
+
+import (
+	"context"
+	"fmt"
+)
+
+func (m *migrations) RunRequiredMigrations(ctx context.Context) error {
+	return m.runMigrations(ctx, m.requiredMigrationIDs)
+}
+
+func (m *migrations) RunDeferredMigrations(ctx context.Context) error {
+	return m.runMigrations(ctx, m.deferredMigrationIDs)
+}
+
+func (m *migrations) runMigrations(ctx context.Context, migrationIDs migrationIDs) error {
+	var hasError = false
+
+	for _, migration := range migrationIDs {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("context canceled: %w", ctx.Err())
+		default:
+		}
+
+		exists, err := m.usecases.downloadMigration.Exists(ctx, migration.id)
+		if err != nil {
+			hasError = true
+			continue
+		}
+
+		if exists {
+			continue
+		}
+
+		m.logger.Info("Start data migration...", "id", migration.id)
+
+		done, err := migration.run(ctx)
+		if err != nil {
+			m.logger.Warn("Failed data migration process '%s'", "id", migration.id, "error", err)
+			hasError = true
+			continue
+		}
+
+		if done {
+			err := m.markMigration(ctx, migration.id)
+			if err != nil {
+				hasError = true
+			}
+		}
+
+		m.logger.Info("Data migration completed", "id", migration.id)
+	}
+
+	if hasError {
+		return fmt.Errorf("errors in the 'downloader usecase' migrations")
+	}
+
+	return nil
+}
