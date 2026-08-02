a2122fe1a5e5265935c6de7703028c5016aafb54
Neosy <neosy.dev@gmail.com>
2026-08-02 23:42:57 +0300

    Fix: Validate media watch interval

--- internal/api/rest/server/internal/handlers/ui/downloader/dto/watch_tracking_request.go
+++ internal/api/rest/server/internal/handlers/ui/downloader/dto/watch_tracking_request.go
@@ -2,6 +2,6 @@ package dto
 
 type MediaItemWatchTrackingRequest struct {
 	PositionMs int    `json:"positionMs" validate:"omitempty,gte=0"`
-	IntervalMs int    `json:"intervalMs" validate:"required,gt=0"`
+	IntervalMs int    `json:"intervalMs" validate:"required,gt=0,lte=11000"`
 	EventType  string `json:"eventType" validate:"omitempty,oneof=pause ended seek heartbeat"`
 }
