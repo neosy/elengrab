8ea531071dc536e4333a769d64e77ce59510aa3f
Neosy <neosy.dev@gmail.com>
2026-09-04 15:30:40 +0300

    Feature: Add media sorting options (Newest, Popular, Oldest). Closes #450

--- internal/app/usecases/migrations/internal/required/init_migrations.go
+++ internal/app/usecases/migrations/internal/required/init_migrations.go
@@ -4,4 +4,5 @@ func (m *migrations) initMigrations() {
 	m.Add("01_move_downloads_to_storage", m.moveDownloadsToStorage)
 	m.Add("02_change_watch_chunk_size", m.changeWatchChunkSizeOnce)
 	m.Add("03_rebuild_watch_stats", m.rebuildWatchStatsOnce)
+	m.Add("04_build_search_index", m.buildSearchIndexOnce)
 }
