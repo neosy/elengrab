bb3cb81dfc2fe3986aae2bbc1c1ef742733fa638
Neosy <neosy.dev@gmail.com>
2026-09-14 18:53:37 +0300

    Feature: Add thumbnail source for YouTube Shorts [L139]

--- internal/app/services/ytdlp/internal/consts/consts.go
+++ internal/app/services/ytdlp/internal/consts/consts.go
@@ -1,6 +1,9 @@
 package consts
 
-import "time"
+import (
+	"slices"
+	"time"
+)
 
 const (
 	YtDlpName = "yt-dlp"
@@ -26,3 +29,14 @@ const (
 	ConcurrentFragmentsDefault = 5
 	MaxTitleLengthInFilename   = 100
 )
+
+var (
+	shortYoutubeThumbnailURLTemplates = [...]string{
+		"https://i.ytimg.com/vi/%s/oardefault.jpg",
+		"https://i.ytimg.com/vi/%s/oar2.jpg",
+	}
+)
+
+func ShortYoutubeThumbnailURLTemplates() []string {
+	return slices.Clone(shortYoutubeThumbnailURLTemplates[:])
+}
--- internal/config/consts.go
+++ internal/config/consts.go
@@ -5,5 +5,5 @@ const (
 	AppName = "Elengrab"
 
 	// Application version.
-	AppVersion = "0.25.4"
+	AppVersion = "0.26.0b"
 )
